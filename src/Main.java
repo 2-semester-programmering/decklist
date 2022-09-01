2eff8106df5246ff9ecda8684429ec497c0f8aa0
clbokea <clbo@kea.dk>
2022-09-01 15:29:44 +0200

    init

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+
+        public static void main(String[] args) {
+                List<String> text = new ArrayList<>(); // user code
+                List<Card> deck = new DeckList<>();
+
+                //deck.add(new Card());
+
+                Object[] arr = new Object[52];
+                for(int i = 0; i <= arr.length; i++){
+                        System.out.println(arr[i]);
+                }
+
+
+        }
+
+}
